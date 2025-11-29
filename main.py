87210b0413159c8281ce5195dc32963cca1957bf
TalhaShahzad-321 <shahzadtalha843@gmail.com>
2025-11-29 13:19:10 +0500

    Deployement Changes

--- main.py
+++ main.py
@@ -3,22 +3,24 @@ from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import ModelTrainerConfig
-from networksecurity.components.model_trainer import ModelTrainer
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig, DataValidationConfig, DataTransformationConfig
-import sys
-if __name__=='__main__':
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig
 
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+ 
+
+import sys
+
+if __name__=='__main__':
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
-
         dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
         data_ingestion=DataIngestion(dataingestionconfig)
-        logging.info("Initiate the Data Ingestionm")
+        logging.info("Initiate the data ingestion")
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
-        
         data_validation_config=DataValidationConfig(trainingpipelineconfig)
         data_validation=DataValidation(dataingestionartifact,data_validation_config)
         logging.info("Initiate the data Validation")
@@ -38,6 +40,8 @@ if __name__=='__main__':
         model_trainer_artifact=model_trainer.initiate_model_trainer()
 
         logging.info("Model Training artifact created")
-
+        
+        
+        
     except Exception as e:
            raise NetworkSecurityException(e,sys)
